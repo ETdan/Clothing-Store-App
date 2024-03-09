adcca059bf7b74ebc0152974a4bbc4001da5c83b
nasifay <nasifayc11@gmail.com>
2024-03-09 19:50:21 +0000

    Modela class and Detail page added

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:app/model/Item/item_manager.dart';
+import 'package:app/screens/details.dart';
 import 'package:app/screens/first-page.dart';
 import 'package:flutter/material.dart';
 
@@ -10,9 +12,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ItemManager items = ItemManager();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: first(),
+      theme: ThemeData(primaryColor: const Color.fromARGB(255, 0, 93, 169)),
+      home: DetailsPage(item: items.featuredItems[0],),
     );
   }
 }
