50c9e7112ed1bbf545343b92f635723f559534da
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-03-17 23:27:57 +0300

    😕😕

--- app/lib/screens/welcome.dart
+++ app/lib/screens/welcome.dart
@@ -25,103 +25,76 @@ class _welcomeState extends State<welcome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.5,
-            child: CarouselSlider.builder(
-              itemCount: 3,
-              itemBuilder: (context, index, realIndex) {
-                return Sbuilder(
-                  index: index,
-                );
-              },
-              options: CarouselOptions(
-                enlargeCenterPage: true,
-                viewportFraction: 1.0,
-                autoPlayAnimationDuration: Duration(microseconds: 50),
-                autoPlay: true,
-                aspectRatio: 130 / 120,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    active_index = index;
-                  });
-                },
-              ),
-            ),
-          ),
-          indicator(
-            index: active_index,
-          ),
-          const Text(
-            'Welcome to Our Shop !',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.black87,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-         const SizedBox(
-            height: 30,
-          ),
-          MaterialButton(
-            height: 60,
-            minWidth: 400,
-            color:const  Color.fromARGB(255, 128, 140, 220),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const signin(),
-                ),
-              );
-            },
-            child: const SizedBox(
-              width: 400,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    'Get Started',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black87,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Icon(Icons.arrow_forward_ios_outlined)
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Sbuilder extends StatelessWidget {
-  final int index;
-  const Sbuilder({
-    super.key,
-    required this.index,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.5,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage(
-          collection().welcomeim[index],
+      body: Stack(children: [
+        PageView(
+          controller: _controller,
+          onPageChanged: (index) {
+            setState(() {
+              onLastPage = (index == 2);
+            });
+          },
+          children: [
+            Page_1(),
+            Page_2(),
+            Page_3(),
+          ],
         ),
-        fit: BoxFit.fill,
-      )),
+
+        // dot indicator
+        Container(
+          alignment: Alignment(0, 0.75),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // skip
+              GestureDetector(
+                  onTap: () {
+                    _controller.jumpToPage(2);
+                  },
+                  child: Text(
+                    'skip',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  )),
+
+              //dot indicator
+              SmoothPageIndicator(controller: _controller, count: 3),
+
+              // done
+              onLastPage
+                  ? GestureDetector(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return signin();
+                        }));
+                      },
+                      child: Text(
+                        'done',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ))
+                  : GestureDetector(
+                      onTap: () {
+                        _controller.nextPage(
+                            duration: Duration(milliseconds: 500),
+                            curve: Curves.easeIn);
+                      },
+                      child: Text(
+                        'next',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      )),
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
